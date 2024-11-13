87701319657bbc1cf742ab73a4f0f9656a1447c1
jamie-codez <cruiseomondi90@gmail.com>
2024-11-13 03:46:17 +0300

    Added entities

---
+++ src/main/java/com/jamie/movies/models/Actor.java
@@ -0,0 +1,59 @@
+package com.jamie.movies.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "actor", schema = "public")
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "actor_id_gen")
+    @SequenceGenerator(name = "actor_id_gen", sequenceName = "actor_actor_id_seq", allocationSize = 1)
+    @Column(name = "actor_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @ColumnDefault("now()")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+}
